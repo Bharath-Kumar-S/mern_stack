cb4dbf90cc4693515610583cd5374a1cf8e63130
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2021-12-05 22:25:52 +0200

    Day 2 react component

---
+++ Day 2/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import "./App.css"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <div className="App-header">
+      <Header />
+      I am Bharath
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ Day 2/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <p>This is Header</p>
+        </div>
+    )
+}
+
+export default Header
