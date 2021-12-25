2b6b76ee800532cb45c393050a69e9a8de99ca35
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2021-12-25 12:45:59 +0200

    add media query for styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  width: 100%; 
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -9,16 +10,20 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
-&:focus {
-  outline: none;
-}
+  @media (min-width: 760px) {
+    width: auto;
+  }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
 `;
 
 // const Button = props => {
