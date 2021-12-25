22a813463e0c3629560b28e1609964a043e6e9f2
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2021-12-25 23:15:32 +0200

    ReactDom create portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,40 @@
 import React from "react";
+import ReactDom from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
-import styles from "./ErrorModal.module.css"
+import styles from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
-    let {title, contents} = props.error;
+  let { title, contents } = props.error;
+
+  const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onClick} />;
+  };
+
+  const Modal = (props) => {
+    return (
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{contents}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.handler}>Close</Button>
+        </footer>
+      </Card>
+    );
+  };
+
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.handler}>
-        <Card className={styles.modal}>
-          <header className={styles.header}>
-            <h2>{title}</h2>
-          </header>
-          <div className={styles.content}>
-            <p>{contents}</p>
-          </div>
-          <footer className={styles.actions}>
-            <Button onClick={props.handler}>Close</Button>
-          </footer>
-        </Card>
-      </div>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onClick={props.handler} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDom.createPortal(<Modal handler={props.handler}/>, document.getElementById("model"))}
+    </React.Fragment>
   );
 };
 
