2b7fb426381c609d63664ba7363986b64445c9b4
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2021-12-26 13:38:39 +0200

    React Task app delete task and task reminder

--- src/App.js
+++ src/App.js
@@ -4,29 +4,48 @@ import Tasks from "./components/Tasks";
 import { useState } from "react";
 
 const App = () => {
-
   const [tasks, setTasks] = useState([
     {
       id: 1,
       text: "Doctor",
       day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
       id: 2,
       text: "Meeting",
       day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
       id: 3,
       text: "Shopping",
       day: "Feb 5th at 2:30pm",
+      reminder: false,
     },
   ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder : !task.reminder}: task));
+  };
 
   return (
     <>
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          toggleReminder={toggleReminder}
+        />
+      ) : (
+        "No Task To Show"
+      )}
     </>
   );
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import {FaTimes} from 'react-icons/fa'
 
-const Tasks = ({tasks}) => {
-    return (
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index} className='task'>
-            <h3>{task.text} <FaTimes style={{color:'red', cursor:'pointer'}}/> </h3>
-            <h6>{task.day}</h6>
-          </li>
-        ))}
-      </ul>
-    );
-}
+const Tasks = ({ tasks, onDelete, toggleReminder }) => {
+  const deleteHandler = (id) => {
+    onDelete(id);
+  };
+
+  const reminderHandler = (id) => {
+    toggleReminder(id);
+  }
+
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <li
+          key={index}
+          className={`task ${task.reminder ? 'reminder' : ''}`}
+          onDoubleClick={() => reminderHandler(task.id)}
+        >
+          <h3>
+            {task.text}
+            <FaTimes
+              style={{ color: "red", cursor: "pointer" }}
+              onClick={() => deleteHandler(task.id)}
+            />
+          </h3>
+          <h6>{task.day}</h6>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Tasks
