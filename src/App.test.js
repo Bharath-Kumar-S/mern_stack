a4cee86e2432f814cc501d5cb04baab2be6a63a9
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2021-12-26 22:48:24 +0200

    Next JS init app

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import Expense from './components/Expense';
-
-test("renders learn react link", () => {
-  render(<Expense />);
-  const linkElement = screen.getAllByText(/Title/i)[0];
-  expect(linkElement).toBeInTheDocument();
-});
