a15d4c05bd3bd91300e229df4b26edc57cfa625c
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2021-12-26 21:40:15 +0200

    React Task app json-server, router,useLocation final commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,95 @@
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctor",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
+  const [addTaskView, setAddTaskView] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const tasksFromServer = await getTasks();
+      setTasks(tasksFromServer);
+    };
+    fetchTasks();
+  }, []);
 
-  const deleteTask = (id) => {
+  const getTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
+
+  const getTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder : !task.reminder}: task));
+  const toggleReminder = async (id) => {
+    const task = await getTask(id);
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ ...task, reminder: !task.reminder }),
+    });
+    const data = await res.json();
+    setTasks(tasks.map((task) => (task.id === id ? { ...data } : task)));
+  };
+
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
+
+  const addTaskViewHandler = () => {
+    setAddTaskView(!addTaskView);
   };
 
   return (
-    <>
-      <Header />
-      {tasks.length > 0 ? (
-        <Tasks
-          tasks={tasks}
-          onDelete={deleteTask}
-          toggleReminder={toggleReminder}
+    <BrowserRouter>
+      <Header addFormView={addTaskViewHandler} addTaskView={addTaskView} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <React.Fragment>
+              {addTaskView && <AddTask addTask={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  toggleReminder={toggleReminder}
+                />
+              ) : (
+                "No Task To Show"
+              )}
+              <Footer />
+            </React.Fragment>
+          }
         />
-      ) : (
-        "No Task To Show"
-      )}
-    </>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
-const Header = ({ title }) => {
-  const onClick = (data) => {
-    console.log('Hello')
+const Header = ({ title, addFormView, addTaskView }) => {
+  const onClick = () => {
+    addFormView();
   };
+
+  const location = useLocation();
 
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick}/>
+      {location.pathname === '/' && (<Button
+        color={addTaskView ? "red" : "green"}
+        text={addTaskView ? "Close" : "Add"}
+        onClick={onClick}
+      />)}
     </header>
   );
 };
